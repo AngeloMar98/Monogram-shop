f2dbdbb088ba28c2b1de1355eb42548baf2a7a47
Angelo <111654683+AngeloMar98@users.noreply.github.com>
2023-08-20 23:22:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const updatePricesCurrency = function () {
             case "EUR":
                 priceCurrency.textContent = "€";
                 priceNumber.textContent = String(Intl.NumberFormat("de-DE").format(fx
-                    .convert(Number((_d = priceNumber === null || priceNumber === void 0 ? void 0 : priceNumber.textContent) === null || _d === void 0 ? void 0 ), {
+                    .convert(Number(_d = priceNumber === null || priceNumber === void 0 ? void 0 : priceNumber.textContent), {
                     from: previousCurrency,
                     to: currCurrencyLong === null || currCurrencyLong === void 0 ? void 0 : currCurrencyLong.textContent,
                 })
